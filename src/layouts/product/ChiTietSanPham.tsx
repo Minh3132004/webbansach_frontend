e348c80cfa75a6e3c7f3eff3871f8722526546a3
Minh3132004 <vubinhminh04@gmail.com>
2025-10-21 11:19:18 +0700

    Front-end: Xây dựng trang xem chi tiết sản phẩm – phần số lượng đặt hàng

--- src/layouts/product/ChiTietSanPham.tsx
+++ src/layouts/product/ChiTietSanPham.tsx
@@ -5,6 +5,7 @@ import { laySachTheoMaSach } from "../../api/SachAPI";
 import HinhAnhSanPham from "./component/HinhAnhSanPham";
 import DanhGiaSanPham from "./component/DanhGiaSanPham";
 import renderRating from "../utils/SaoXepHang";
+import { BagFill } from "react-bootstrap-icons";
 
 
 
@@ -25,10 +26,37 @@ const ChitietSanPham: React.FC = () => {
         maSachNumber = 0;
     }
 
-
     const [sach , setSach] = useState<SachModel | null>(null);
     const [dangTaiDuLieu, setDangTaiDuLieu] = useState(true);
     const [baoLoi , setBaoLoi] = useState(null);
+    const [soLuong , setSoLuong] = useState(1);
+
+    const tangSoLuong = () => {
+        if(sach && sach.soLuong && soLuong < sach.soLuong){
+            setSoLuong(soLuong + 1);
+        }
+    }
+    const giamSoLuong = () => {
+        if(sach && sach.soLuong && soLuong > 1){
+            setSoLuong(soLuong - 1);
+        }
+    }
+    
+    const handleSoLuong= (event: React.ChangeEvent<HTMLInputElement>) => {
+        const soLuongNumber = parseInt(event.target.value);
+        if(soLuongNumber >= 1 && sach && sach.soLuong && soLuongNumber <= sach.soLuong){
+            setSoLuong(soLuongNumber);
+        }
+    }
+    
+    const handleMuaNgay = () => {
+        
+    
+    }
+    const handleThemVaoGioHang = () => {
+        
+    
+    }
 
     useEffect(()=> {
             laySachTheoMaSach(maSachNumber)
@@ -80,6 +108,22 @@ const ChitietSanPham: React.FC = () => {
                         <h3 className="text-danger fw-bold">{sach.giaBan?.toLocaleString('vi-VN')} VNĐ</h3>
                     </div>
 
+                    <div className="col-4">
+                        <div>
+                            <div className="mb-2">Số lượng</div>
+                            <div className="d-flex align-items-center">
+                                <button className="btn btn-outline-secondary" onClick={giamSoLuong}>-</button>
+                                <input type="number" className="form-control text-center" onChange={handleSoLuong} value={soLuong} />
+                                <button className="btn btn-outline-secondary" onClick={tangSoLuong}>+</button>
+                            </div>
+                            <span>
+                                {
+                                    <span>Số tiền tạm tính : {(soLuong * (sach.giaBan || 0)).toLocaleString('vi-VN')} VNĐ</span>
+                                }
+                            </span>
+                        </div>
+                    </div>
+
                     <div className="mb-4">
                         <h5 className="mb-2">Mô tả sản phẩm:</h5>
                         <p className="text-muted">{sach.moTa}</p>
@@ -88,7 +132,11 @@ const ChitietSanPham: React.FC = () => {
                     <div className="border rounded p-3 bg-light">
                         <h5 className="mb-3">Đặt hàng</h5>
                         <div className="d-grid gap-2">
-                            <button className="btn btn-danger btn-lg">
+                            <button className="btn btn-danger btn-lg" onClick={handleMuaNgay}>
+                                <BagFill className="me-2" />
+                                Mua ngay
+                            </button>
+                            <button className="btn btn-danger btn-lg" onClick={handleThemVaoGioHang}>
                                 <i className="fas fa-shopping-cart me-2"></i>
                                 Thêm vào giỏ hàng
                             </button>
