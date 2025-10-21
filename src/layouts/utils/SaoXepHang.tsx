0bf7a3a6cea755856f9e9a01d23e1aed8d50aec0
Minh3132004 <vubinhminh04@gmail.com>
2025-10-21 08:37:46 +0700

    FrontEnd : Fix render rating

--- src/layouts/utils/SaoXepHang.tsx
+++ src/layouts/utils/SaoXepHang.tsx
@@ -3,7 +3,7 @@ import { Star, StarFill } from "react-bootstrap-icons";
 const renderRating = (diem : number) => {
         const star = [];
         for(let i = 0 ; i < 5 ; i++){
-            if(i <= diem){
+            if(i < diem){
                 star.push(<StarFill className="text-warning"/>)
             }else{
                 star.push(<Star className="text-secondary"/>)
